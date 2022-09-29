65e9a12442c9777392dc3e2584f81009d796a0a5
hlong-21 <quangtk@concrete-corp.com>
2022-09-29 14:17:25 +0700

    edit email

--- dist/mail.php
+++ dist/mail.php
@@ -34,13 +34,13 @@ if (version_compare(PHP_VERSION, '5.1.0', '>=')) {//PHP5.1.0以上の場合の�
 $site_top = "/";
 
 //管理者のメールアドレス ※メールを受け取るメールアドレス(複数指定する場合は「,」で区切ってください 例 $to = "aa@aa.aa,bb@bb.bb";)
-// $to = "wakayama-h@dym.jp";
-$to = "quangtk@concrete-corp.com";
+$to = "wakayama-h@dym.jp";
+// $to = "quangtk@concrete-corp.com";
 
 //自動返信メールの送信元メールアドレス
 //必ず実在するメールアドレスでかつ出来る限り設置先サイトのドメインと同じドメインのメールアドレスとすることを強く推奨します
-// $from = "wakayama-h@dym.jp";
-$from = "quangtk@concrete-corp.com";
+$from = "wakayama-h@dym.jp";
+// $from = "quangtk@concrete-corp.com";
 
 //フォームのメールアドレス入力箇所のname属性の値（name="○○"　の○○部分）
 $Email = "ご担当者様メールアドレス";
